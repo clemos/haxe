06d4825df3aabbef5575ce837508769117d466e1
Clément Charmet <clement.charmet@ebuzzing.com>
2014-11-26 13:42:35 +0100

    fix for ocaml4 https://groups.google.com/forum/#!topic/haxelang/evhY_bl80M8

--- libs/extlib/extHashtbl.mli
+++ libs/extlib/extHashtbl.mli
@@ -1,7 +1,7 @@
-(* 
+(*
  * ExtHashtbl - extra functions over hashtables.
  * Copyright (C) 2003 Nicolas Cannasse
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -17,7 +17,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *)
- 
+
 (** Extra functions over hashtables. *)
 
 module Hashtbl :
@@ -64,16 +64,16 @@ module Hashtbl :
 		all the values *)
 
 	val length : ('a,'b) t -> int
-	(** Return the number of elements inserted into the Hashtbl 
+	(** Return the number of elements inserted into the Hashtbl
 		(including duplicates) *)
-	
+
 	(** {6 Older Functions} *)
 
 	(** Please refer to the Ocaml Manual for documentation of these
 		functions. (note : functor support removed to avoid code
 		duplication). *)
 
-	val create : int -> ('a, 'b) t
+	val create : ?random:bool -> int -> ('a, 'b) t
 	val clear : ('a, 'b) t -> unit
 	val add : ('a, 'b) t -> 'a -> 'b -> unit
 	val copy : ('a, 'b) t -> ('a, 'b) t
